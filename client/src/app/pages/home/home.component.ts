890ad09a16e0e5b2a696d16bfd0b10181c0a1c0d
Jatin Chouhan <jatinchouhan1515@gmail.com>
2024-08-03 13:56:19 +0530

    Created a search bar

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
+import axios from 'axios';
 
 @Component({
   selector: 'app-home',
@@ -7,18 +8,36 @@ import { ProductsService } from '../../services/products.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  constructor(private productService : ProductsService){}
+  constructor(){}
 
   productList : any;
+  selectedCategory: string = 'Movie';
+  token : string | null = "";
+  page : number = 1;
 
   ngOnInit(){
-    this.getProduct()
+
+    this.token = localStorage.getItem("token");
+
+    this.getMovieList()
   }
 
-  getProduct(){
-    this.productService.getProductList().subscribe(res =>{
-      this.productList = res
-      console.log(this.productList);
-    })
+  getMovieList = async()=>{
+        try{
+          let result = axios.get('https://flet-nix-backend.vercel.app/api/data/movies'+`?Page=${this.page}`,{
+            headers :{
+              "Authorization" : "Bearer " + this.token
+            }
+          });
+
+          console.log(result);
+        }
+        catch(err){
+          console.log("Error Fetching Moveis",err);
+        }
+  }
+
+  onCategoryChange(event: any) {
+    this.selectedCategory = event.target.value;
   }
 }
